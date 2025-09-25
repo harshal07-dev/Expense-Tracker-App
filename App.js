4ae310d4533fb1fa922c9f4a18b5f03fb5efa5c4
harshal07-dev <harshalsys845@gmail.com>
2025-09-25 20:45:49 +0530

    added  navigation

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/Navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <AppNavigator/>
+   </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -1,13 +1,20 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "../screens/Home.js"
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import Home from "../screens/Home"
+// import Create from "../screens/Create";
+// import Insights from "../screens/Insights";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import Profile from "../screens/Profile";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Home from "../screens/Home";
 import Create from "../screens/Create";
 import Insights from "../screens/Insights";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Profile from "../screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
-
 function BottomTabs () {
     return (
         <Tab.Navigator>
@@ -17,8 +24,12 @@ function BottomTabs () {
         </Tab.Navigator>
     )
 }
+
 export default function AppNavigator() {
-    <Stack.Navigator>
-        <Stack.Screen name="BottomTabs" component={BottomTabs}/>
-    </Stack.Navigator>
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="BottomTabs" component={BottomTabs}/>
+            <Stack.Screen name="Profile" component={Profile}/>
+        </Stack.Navigator>
+    )
 }
